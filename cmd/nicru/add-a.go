7f19c1ba8f6e2e87b45d9d7964cfc8791e35485b
Paulstrong <pgusev@team.amocrm.com>
2022-05-09 02:28:50 +0300

    develop cmd

--- cmd/nicru/add-a.go
+++ cmd/nicru/add-a.go
@@ -17,9 +17,7 @@ func addACmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   `add-a`,
 		Short: `добавление A-записей`,
-		Long: `oauth2 ключи нужно получить в ЛК nic.ru - https://www.nic.ru/manager/oauth.cgi?step=oauth.app_register
-имя DNS-сервиса можно посмотреть по адресу https://www.nic.ru/manager/services.cgi?step=srv.my_dns&view.order_by=domain
-`,
+
 		Run: func(cmd *cobra.Command, args []string) {
 			//инициализируем клиента
 			client := api.NewClient(config)
@@ -33,22 +31,8 @@ func addACmd() *cobra.Command {
 					fmt.Printf("Added A-record: %s -> %s\n", record.Name, record.A.String())
 				}
 			}
-			//после создания записей, нужно закоммитить результат, чтобы DNS уехал в "прод"
-			if _, err := client.CommitZone(); err != nil {
-				fmt.Printf("Commit error: %s\n", err.Error())
-			} else {
-				fmt.Printf("Zone committed\n")
-			}
 		},
 	}
-	//создаём флаги для заполнения конфига
-	cmd.PersistentFlags().StringVar(&config.Credentials.OAuth2.ClientID, `oauth2-client-id`, ``, `oauth2 client id`)
-	cmd.PersistentFlags().StringVar(&config.Credentials.OAuth2.SecretID, `oauth2-secret-id`, ``, `oauth2 secret id`)
-	cmd.PersistentFlags().StringVar(&config.Credentials.Username, `username`, ``, `логин от ЛК nic.ru (******/NIC-D)`)
-	cmd.PersistentFlags().StringVar(&config.Credentials.Password, `password`, ``, `пароль от nic.ru`)
-	cmd.PersistentFlags().StringVar(&config.ZoneName, `zone-name`, `example.com`, `имя DNS-зоны`)
-	cmd.PersistentFlags().StringVar(&config.DnsServiceName, `service-name`, `EXAMPLE`, `имя DNS-сервиса`)
-	cmd.PersistentFlags().StringVar(&config.CachePath, `cache-path`, `/tmp/.nic.ru.token`, `путь до файла, где будет храниться авторизация от API`)
 	//создаём флаги для указания имён создаваемых записей, ipv4-таргета и TTL
 	cmd.PersistentFlags().StringSliceVar(&names, `names`, []string{}, `имена, которые нужно создать`)
 	cmd.PersistentFlags().StringVar(&target, `target`, ``, `куда нужно отправить имена (например, 1.2.3.4)`)
--- cmd/nicru/commit.go
+++ cmd/nicru/commit.go
@@ -9,11 +9,8 @@ import (
 func commitCmd() *cobra.Command {
 
 	cmd := &cobra.Command{
-		Use:   `add-a`,
-		Short: `добавление A-записей`,
-		Long: `oauth2 ключи нужно получить в ЛК nic.ru - https://www.nic.ru/manager/oauth.cgi?step=oauth.app_register
-имя DNS-сервиса можно посмотреть по адресу https://www.nic.ru/manager/services.cgi?step=srv.my_dns&view.order_by=domain
-`,
+		Use:   `commit`,
+		Short: `фиксация изменений`,
 		Run: func(cmd *cobra.Command, args []string) {
 			//инициализируем клиента
 			client := api.NewClient(config)
@@ -25,14 +22,6 @@ func commitCmd() *cobra.Command {
 			}
 		},
 	}
-	//создаём флаги для заполнения конфига
-	cmd.PersistentFlags().StringVar(&config.Credentials.OAuth2.ClientID, `oauth2-client-id`, ``, `oauth2 client id`)
-	cmd.PersistentFlags().StringVar(&config.Credentials.OAuth2.SecretID, `oauth2-secret-id`, ``, `oauth2 secret id`)
-	cmd.PersistentFlags().StringVar(&config.Credentials.Username, `username`, ``, `логин от ЛК nic.ru (******/NIC-D)`)
-	cmd.PersistentFlags().StringVar(&config.Credentials.Password, `password`, ``, `пароль от nic.ru`)
-	cmd.PersistentFlags().StringVar(&config.ZoneName, `zone-name`, `example.com`, `имя DNS-зоны`)
-	cmd.PersistentFlags().StringVar(&config.DnsServiceName, `service-name`, `EXAMPLE`, `имя DNS-сервиса`)
-	cmd.PersistentFlags().StringVar(&config.CachePath, `cache-path`, `/tmp/.nic.ru.token`, `путь до файла, где будет храниться авторизация от API`)
 
 	return cmd
 }
